100979edcf799bf340705d355739ebf7f642f59d
Macky Dieng <elmackydieng@live.fr>
2015-12-03 16:47:42 +0100

    Suite des views Dog et GPS

--- src/view/ViewGPS.java
+++ src/view/ViewGPS.java
@@ -1,7 +1,25 @@
 package view;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ViewGPS extends JFrame {
-
+	private String currentState;
+	
+	public ViewGPS() { 
+		this.currentState = "Début";
+		this.setLayout(new GridLayout(200,200));
+		this.add(new JLabel(this.currentState));
+		this.setVisible(true);
+	}
+	public void setCurrentState(String state)
+	{
+		this.currentState = state;
+	}
+	public String getCurrentState()
+	{
+		return this.currentState;
+	}
 }
