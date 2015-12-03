7d2ae6ad8f797aece29767edb42f10f378b6f808
Mameri Helen <21001417@frene.etu.info.unicaen.fr>
2015-12-03 15:53:51 +0100

    Création de TimeController

---
+++ src/controller/TimeController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import model.SimpleChangeListener;
+import model.Time;
+
+public class TimeController extends JFrame implements ChangeListener, SimpleChangeListener, ActionListener
+{
+	private Slider slide;
+	private Button bouton;
+	private Time time;
+	private JLabel label;
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stateChanged(Object source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
